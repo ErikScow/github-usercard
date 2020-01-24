93a44d197f5d8212e04baf5db432b7a0d8f0ebc0
Erik Scow <erikscow@gmail.com>
2020-01-24 11:41:29 -0800

    Added first axios data request. Created component for usercard. Passed my data into the component function. Added new cards for lambda instructors.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,7 +2,8 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
-
+const myData = axios.get('https://api.github.com/users/ErikScow')
+console.log(myData)
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -13,6 +14,11 @@
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+const cards = document.querySelector('.cards')
+
+myData.then(response => {
+  cards.appendChild(cardCreator(response.data))
+})
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -24,7 +30,21 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [`tetondan`, `dustinmyers`, `justsml`, `luishrd`, `bigknell`
+];
+
+const newData = followersArray.map(item => {
+  const newUser = `https://api.github.com/users/${item}`
+  return newUser
+})
+
+newData.forEach(item => {
+  const newCard = axios.get(`${item}`)
+  newCard.then(response => {
+    cards.appendChild(cardCreator(response.data))
+  })
+})
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +65,54 @@ const followersArray = [];
 </div>
 
 */
+
+const cardCreator = (data) => {
+  
+//create elements
+const cardDiv = document.createElement('div')
+const image = document.createElement('img')
+const infoDiv = document.createElement('div')
+const name = document.createElement('h3')
+const username = document.createElement('p')
+const location = document.createElement('p')
+const profile = document.createElement('p')
+const profileLink = document.createElement('a')
+const followerCount = document.createElement('p')
+const followingCount = document.createElement('p')
+const bio = document.createElement('p')
+
+//nesting elements
+cardDiv.appendChild(image)
+cardDiv.appendChild(infoDiv)
+infoDiv.appendChild(name)
+infoDiv.appendChild(username)
+infoDiv.appendChild(location)
+infoDiv.appendChild(profile)
+infoDiv.appendChild(followerCount)
+infoDiv.appendChild(followingCount)
+infoDiv.appendChild(bio)
+profile.appendChild(profileLink)
+
+//adding classes
+cardDiv.classList.add('card')
+infoDiv.classList.add('card-info')
+name.classList.add('name')
+username.classList.add('username')
+
+//adding content
+image.src = data.avatar_url
+name.textContent = `Name: ${data.name}`
+username.textContent = `Username: ${data.login}`
+location.textContent = `Location: ${data.location}`
+profileLink.href = `${data.html_url}`
+profileLink.textContent = `Profile`
+followerCount.textContent = `Followers: ${data.followers}`
+followingCount.textContent = `Following: ${data.following}`
+bio.textContent = `Bio: ${data.bio}`
+
+return cardDiv
+
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
